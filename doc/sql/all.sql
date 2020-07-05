a56b802481d633322901d06e582ea643b2ab666a
nemoeckel <stephanie_eckel@126.com>
2020-07-05 22:17:29 +0800

    1.增加dto层，data transfer object，用于controller和service层 2.增加ChapterDto 3.修改chapterService,将返回Chpater类改为返回ChapterDto类

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -1,1 +1,10 @@
-select * from test;
+drop table if exists chapter;
+create table chapter(
+                        id char(8) not null comment 'ID',
+                        course_id char(8) comment '课程ID',
+                        name varchar(50) comment '名称',
+                        primary key (id)
+)engine=innodb default charset=utf8mb4 comment ='大章';
+
+insert into chapter values ('00000000','00000000','测试大章一');
+insert into chapter values ('00000001','00000001','测试大章二');
