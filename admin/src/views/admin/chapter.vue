2af223c74e5df6cd1f0f2dd53ba37516dfec450d
nemoeckel <stephanie_eckel@126.com>
2020-07-09 21:03:44 +0800

    1.使用gateway进行路由转发，vue页面只访问gateway

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -89,7 +89,7 @@
         methods:{
             list(){
                 let _this = this;
-                _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+                _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
                     console.log(response);
                     _this.chapters = response.data;
                 })
