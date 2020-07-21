c31d172a75214d49c719fc9d2ddb768a3e63f6cb
nemoeckel <stephanie_eckel@126.com>
2020-07-21 21:58:53 +0800

    1.增加删除大章的功能

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -51,4 +51,8 @@ public class ChapterService {
     private void update(Chapter chapter) {
         chapterMapper.updateByPrimaryKey(chapter);
     }
+
+    public void delete(String id) {
+        chapterMapper.deleteByPrimaryKey(id);
+    }
 }
