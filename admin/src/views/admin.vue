d5f24438500a7ccc6d1d7cb3255d7cac8f62d447
nemoeckel <stephanie_eckel@126.com>
2020-07-26 15:48:21 +0800

    1.生成器开发：增加生成vue界面代码 2.小节管理增删改查测试成功

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -415,6 +415,16 @@
                                 <b class="arrow"></b>
                             </li>
                         </ul>
+                        <ul class="submenu">
+                            <li class="" id="business-section-sidebar">
+                                <router-link to="/business/section">
+                                    <i class="menu-icon fa fa-caret-right"></i>
+                                    小节管理
+                                </router-link>
+
+                                <b class="arrow"></b>
+                            </li>
+                        </ul>
                     </li>
                 </ul><!-- /.nav-list -->
 
