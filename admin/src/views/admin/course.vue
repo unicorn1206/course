51a2914cd5b1f4478352428e53685b5b40a100f1
nemoeckel <stephanie_eckel@126.com>
2020-08-16 16:49:20 +0800

    1.利用setInterval完成自动保存功能

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -119,6 +119,11 @@
                         <form class="form-horizontal">
                             <div class="form-group">
                                 <div class="col-lg-12">
+                                    {{saveContentLabel}}
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-lg-12">
                                     <div id="content"></div>
                                 </div>
                             </div>
@@ -197,7 +202,8 @@
                 COURSE_CHARGE: COURSE_CHARGE,
                 COURSE_STATUS: COURSE_STATUS,
                 categorys: [],
-                tree:{}
+                tree:{},
+                saveContentLabel:""
             }
         },
         mounted:function () {
@@ -375,6 +381,7 @@
                 });
                 //现清空历史内容
                 $('#content').summernote('code','');
+                _this.saveContentLabel = "";
                 Loading.show();
                 _this.$ajax.get(process.env.VUE_APP_SERVER +  '/business/admin/course/find-content/' + id
                 ).then((response)=>{
@@ -385,6 +392,14 @@
                         if(resp.content){
                             $('#content').summernote('code',resp.content.content);
                         }
+                        //定时自动保存
+                        let saveContentInterval = setInterval(function () {
+                            _this.saveContent();
+                        },5000);
+                        //关闭内容框时，清空自动保存任务
+                        $('#course-content-modal').on('hidden.bs.modal',function (e) {
+                            clearInterval(saveContentInterval);
+                        });
                     }else{
                         Toast.warning(resp.message);
                     }
@@ -404,7 +419,8 @@
                     Loading.hide();
                     let resp = response.data;
                     if(resp.success){
-                        Toast.success("内容保存成功");
+                        //Toast.success("内容保存成功");
+                        _this.saveContentLabel = "最后保存时间：" + Tool.dateFormat("mm:ss");
                     }else{
                         Toast.warning(resp.message);
                     }
