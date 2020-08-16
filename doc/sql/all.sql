d7106ddf98ce673f3d578566de51bd03749497bb
nemoeckel <stephanie_eckel@126.com>
2020-08-16 12:59:25 +0800

    1.课程内容表设计与基本代码生成

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -1,3 +1,10 @@
+drop table if exists course_content;
+create table course_content(
+                                id char(8) not null default '' comment '课程id',
+                                content mediumtext not null comment '课程内容',
+                                primary key (id)
+)engine=innodb default charset=utf8mb4 comment ='课程内容';
+
 drop table if exists course_category;
 create table course_category(
     id char(8) not null default '' comment 'id',
