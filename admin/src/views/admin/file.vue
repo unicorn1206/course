ee778f06c19e660d58b0f1f9263c37aacf3b73a2
nemoeckel <stephanie_eckel@126.com>
2020-09-16 22:53:32 +0800

    1.文件上传功能开发：去掉文件管理的新增、修改、删除功能

--- admin/src/views/admin/file.vue
+++ admin/src/views/admin/file.vue
@@ -1,11 +1,6 @@
 <template>
     <div>
-        <p>
-            <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-                <i class="ace-icon fa fa-edit"></i>
-                新增
-            </button>
-            &nbsp;
+        <p>&nbsp;
             <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
                 <i class="ace-icon fa fa-refresh"></i>
                 刷新
@@ -17,56 +12,6 @@
 <!--        <pagination ref="pagination" v-bind:list="list"></pagination>-->
         <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="6"></pagination>
 
-        <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">表单</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form class="form-horizontal">
-                                                        <div class="form-group">
-                                <label class="col-sm-2 control-label">相对路径</label>
-                                <div class="col-sm-10">
-                                    <input v-model="file.path" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label">文件名</label>
-                                <div class="col-sm-10">
-                                    <input v-model="file.name" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label">后缀</label>
-                                <div class="col-sm-10">
-                                    <input v-model="file.suffix" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label">大小</label>
-                                <div class="col-sm-10">
-                                    <input v-model="file.size" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label">用途</label>
-                                <div class="col-sm-10">
-                                    <select v-model="file.use" class="form-control">
-                                        <option v-for="o in FILE_USE" v-bind:key="o.key" v-bind:value="o.key">{{o.value}}</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                        <button type="button" class="btn btn-primary" v-on:click="save()">保存</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
 
         <table id="simple-table" class="table  table-bordered table-hover">
                     <thead>
@@ -77,9 +22,6 @@
                             <th>后缀</th>
                             <th>大小</th>
                             <th>用途</th>
-
-
-                        <th>操作</th>
                     </tr>
                     </thead>
 
@@ -91,52 +33,6 @@
                         <td>{{file.suffix}}</td>
                         <td>{{file.size}}</td>
                         <td>{{FILE_USE | optionKV(file.use)}}</td>
-                        <td>
-                            <div class="hidden-sm hidden-xs btn-group">
-
-                                <button v-on:click="edit(file)" class="btn btn-xs btn-info">
-                                    <i class="ace-icon fa fa-pencil bigger-120"></i>
-                                </button>
-
-                                <button v-on:click="del(file.id)" class="btn btn-xs btn-danger">
-                                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                </button>
-                            </div>
-
-                            <div class="hidden-md hidden-lg">
-                                <div class="inline pos-rel">
-                                    <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                                        <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                                    </button>
-
-                                    <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                                        <li>
-                                            <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                                                                                <span class="blue">
-                                                                                    <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                                                                                </span>
-                                            </a>
-                                        </li>
-
-                                        <li>
-                                            <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                                                                                <span class="green">
-                                                                                    <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                                                                                </span>
-                                            </a>
-                                        </li>
-
-                                        <li>
-                                            <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                                                                                <span class="red">
-                                                                                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                                                                </span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </td>
                     </tr>
                     </tbody>
                 </table>
@@ -164,25 +60,6 @@
             _this.list(1);
         },
         methods:{
-            /**
-             * 点击【新增】
-             */
-            add(){
-                let _this = this;
-                _this.file = {};
-                $('#form-modal').modal('show');
-            },
-            /**
-             * 点击【编辑】
-             */
-            edit(file){
-                let _this = this;
-                //数据双向绑定
-                // _this.file = file;
-                //取消数据双向绑定，在编辑框输入值，不影响列表内的值
-                _this.file = $.extend({},file);//将file对象复制到一个空对象{}
-                $('#form-modal').modal('show');
-            },
 
             /**
              * 列表查询
@@ -199,60 +76,10 @@
                     _this.files = resp.content.list;
                     _this.$refs.pagination.render(page,resp.content.total);//重新渲染当前组件，所选页数的底色
                 })
-            },
+            }
 
-            /**
-             * 点击【保存】
-             */
-            save(){
-                let _this = this;
-                // let file ={
-                //     couseId:this.courseId,
-                //     name:this.name
-                // }
-                // 保存校验
-                if (1 != 1
-                    || !Validator.require(_this.file.path, "相对路径")
-                    || !Validator.length(_this.file.path, "相对路径", 1, 100)
-                    || !Validator.length(_this.file.name, "文件名", 1, 100)
-                    || !Validator.length(_this.file.suffix, "后缀", 1, 10)
-                ) {
-                    return;
-                }
-          Loading.show();
-          _this.$ajax.post(process.env.VUE_APP_SERVER +  '/file/admin/file/save', _this.file
-          ).then((response)=>{
-              Loading.hide();
-              let resp = response.data;
-              if(resp.success){
-                  $('#form-modal').modal('hide');
-                  _this.list(1);
-                  Toast.success('保存成功');
-              }else{
-                  Toast.warning(resp.message);
-              }
 
-          })
-      },
-      /**
-       * 点击【删除】
-       */
-      del(id){
-          let _this = this;
-          Confirm.show("删除文件后不可恢复，确认删除？",function () {
-              Loading.show();
-              _this.$ajax.delete(process.env.VUE_APP_SERVER +  '/file/admin/file/delete/' + id
-              ).then((response)=>{
-                  Loading.hide();
-                  let resp = response.data;
-                  if(resp.success){
-                      _this.list(1);
-                      Toast.success('删除成功');
-                  }
-              })
-          });
 
-      }
   }
 }
 </script>
