1a9588fa1161a9ece781f4073893e274d2cf5123
unicorn1206 <Stephanie_eckel@126.com>
2020-10-02 23:21:20 +0800

    1.大文件断点续传与极速秒传：分片合并测试成功

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -10,14 +10,14 @@ import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 @RestController
@@ -76,6 +76,43 @@ public class UploadController {
         ResponseDto responseDto = new ResponseDto();
         fileDto.setPath(FILE_DOMAIN + path);
         responseDto.setContent(fileDto);
+        return responseDto;
+    }
+
+    @GetMapping("/merge")
+    public ResponseDto merger() throws Exception {
+        File newFile = new File(FILE_PATH + "course/test123.mp4");
+        FileOutputStream outputStream = new FileOutputStream(newFile,true);//true:文件追加写入
+        FileInputStream fileInputStream = null;//分片文件
+        byte[] byt = new byte[10 * 1024 * 1024];
+        int len;
+
+        try{
+            //读取第一个分片
+            fileInputStream = new FileInputStream(new File(FILE_PATH + "course/test123.mp4"));
+            while((len=fileInputStream.read(byt)) != -1){
+                outputStream.write(byt,0,len);
+            }
+
+            //读取第二个分片
+            fileInputStream = new FileInputStream(new File(FILE_PATH + "course/test123.mp4"));
+            while((len=fileInputStream.read(byt)) != -1){
+                outputStream.write(byt,0,len);
+            }
+        }catch (Exception e){
+            LOG.error("文件合并异常",e);
+        }finally {
+            try {
+                if(fileInputStream != null){
+                    fileInputStream.close();
+                }
+                outputStream.close();
+                LOG.info("IO流关闭");
+            }catch (Exception e){
+                LOG.error("IO流关闭",e);
+            }
+        }
+        ResponseDto responseDto = new ResponseDto();
         return responseDto;
     }
 }
