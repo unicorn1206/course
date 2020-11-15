7037ef342514925540c52457e80a8e6f530805f2
nemoeckel <stephanie_eckel@126.com>
2020-11-15 16:24:06 +0800

    1.用户管理与登录：在请求headers中统一增加token

--- admin/src/main.js
+++ admin/src/main.js
@@ -5,10 +5,9 @@ import axios from 'axios'
 import filter from './filter/filter'
 
 Vue.config.productionTip = false;
-//Vue组件的全局变量。可以在任意vue组件中，使用this.xxx来获取这个值，$代表vue全局属性的约定
 Vue.prototype.$ajax = axios;
 
-//解决每次ajax请求，对应的sessionId不一致的问题
+// 解决每次ajax请求，对应的sessionId不一致的问题
 axios.defaults.withCredentials = true;
 
 /**
@@ -16,11 +15,11 @@ axios.defaults.withCredentials = true;
  */
 axios.interceptors.request.use(function (config) {
   console.log("请求：", config);
-  // let token = Tool.getLoginUser().token;
-  // if (Tool.isNotEmpty(token)) {
-  //   config.headers.token = token;
-  //   console.log("请求headers增加token:", token);
-  // }
+  let token = Tool.getLoginUser().token;
+  if (Tool.isNotEmpty(token)) {
+    config.headers.token = token;
+    console.log("请求headers增加token:", token);
+  }
   return config;
 }, error => {});
 axios.interceptors.response.use(function (response) {
@@ -28,32 +27,31 @@ axios.interceptors.response.use(function (response) {
   return response;
 }, error => {});
 
-
-//全局过滤器
+// 全局过滤器
 Object.keys(filter).forEach(key => {
-  Vue.filter(key,filter[key]);
+  Vue.filter(key, filter[key])
+});
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    return item.meta.loginRequire
+  })) {
+    let loginUser = Tool.getLoginUser();
+    if (Tool.isEmpty(loginUser)) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
 
-console.log("环境:" + process.env.NODE_ENV);
-
-//路由登录拦截
-router.beforeEach((to,from,next) => {
-  //要不要对meta.loginRequire属性做登录拦截
-  if(to.matched.some(function (item){
-    return item.meta.loginRequire;
-  })){
-    let loginUser = Tool.getLoginUser();
-    if(Tool.isEmpty(loginUser)){
-      next("/login");
-    }else{
-      next();
-    }
-  }else{
-    next();
-  }
-});
+console.log("环境：", process.env.NODE_ENV);
