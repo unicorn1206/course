d7c36f855f8d014df52a0dd7eb99d598c0ecc5d0
nemoeckel <stephanie_eckel@126.com>
2020-11-23 21:53:43 +0800

    1.通用权限管理：资源树的显示

--- admin/src/views/admin/resource.vue
+++ admin/src/views/admin/resource.vue
@@ -14,89 +14,16 @@
                     保存
                 </button>
             </div>
-            <div class="col-md-6"></div>
+            <div class="col-md-6">
+                <ul id="tree" class="ztree"></ul>
+            </div>
         </div><br>
 
-        <!--ref属性：组件别名,v-bind,绑定一个方法，左边list为子组件内部定义，暴露给外部的一个回调方法
-        ，右边list是当前组件的list方法，作用：点击按钮的时候要执行什么方法-->
-<!--        <pagination ref="pagination" v-bind:list="list"></pagination>-->
-        <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="6"></pagination>
-
-        <table id="simple-table" class="table  table-bordered table-hover">
-                    <thead>
-                    <tr>
-                            <th>id</th>
-                            <th>名称</th>
-                            <th>页面</th>
-                            <th>请求</th>
-                            <th>父id</th>
-                        <th>操作</th>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-                    <tr v-for="resource in resources" v-bind:key="resource.id">
-                        <td>{{resource.id}}</td>
-                        <td>{{resource.name}}</td>
-                        <td>{{resource.page}}</td>
-                        <td>{{resource.request}}</td>
-                        <td>{{resource.parent}}</td>
-                        <td>
-                            <div class="hidden-sm hidden-xs btn-group">
-
-                                <button v-on:click="edit(resource)" class="btn btn-xs btn-info">
-                                    <i class="ace-icon fa fa-pencil bigger-120"></i>
-                                </button>
-
-                                <button v-on:click="del(resource.id)" class="btn btn-xs btn-danger">
-                                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                </button>
-                            </div>
-
-                            <div class="hidden-md hidden-lg">
-                                <div class="inline pos-rel">
-                                    <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                                        <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                                    </button>
-
-                                    <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                                        <li>
-                                            <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                                                                                <span class="blue">
-                                                                                    <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                                                                                </span>
-                                            </a>
-                                        </li>
-
-                                        <li>
-                                            <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                                                                                <span class="green">
-                                                                                    <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                                                                                </span>
-                                            </a>
-                                        </li>
-
-                                        <li>
-                                            <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                                                                                <span class="red">
-                                                                                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                                                                </span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
     </div>
 </template>
 
 <script>
-    import Pagination from '../../components/pagination'//引入子组件2-1
     export default {
-        components:{Pagination},//引入子组件2-2
         name: "system-resource",
         data:function(){
             return{
@@ -104,31 +31,44 @@
                 // courseId:"",
                 // name:'',
                 resources:[],//列表展示
-                resourceStr:''
+                resourceStr:'',
+                tree:{}
             }
         },
         mounted:function () {
             //this.$parent.activeSideBar("system-resource-sidebar");
             let _this = this;
-            _this.$refs.pagination.size=5;
-            _this.list(1);
+            _this.list();
         },
         methods:{
+            initTree() {
+                let _this = this;
+                let setting = {
+                    data: {
+                        simpleData: {
+                            idKey: "id",
+                            pIdKey: "parent",
+                            rootPId: ""
+                        }
+                    }
+                };
 
+                let zNodes = _this.resources;
+                _this.tree = $.fn.zTree.init($("#tree"), setting, zNodes);
+                _this.tree.expandAll(true);
+            },
             /**
              * 列表查询
              */
-            list(page){
+            list(){
                 let _this = this;
                 Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER +  '/system/admin/resource/list',{
-                    page:page,
-                    size:_this.$refs.pagination.size//获取组件内部的size变量
-                }).then((response)=>{
+                _this.$ajax.get(process.env.VUE_APP_SERVER +  '/system/admin/resource/load-tree').then((response)=>{
                     Loading.hide();
                     let resp = response.data;
-                    _this.resources = resp.content.list;
-                    _this.$refs.pagination.render(page,resp.content.total);//重新渲染当前组件，所选页数的底色
+                    _this.resources = resp.content;
+                    //初始化树
+                    _this.initTree();
                 })
             },
 
