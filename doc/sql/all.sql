b7062b575f87b6283d5090137e16c75667f0b652
nemoeckel <stephanie_eckel@126.com>
2020-11-24 21:32:18 +0800

    1.通用权限管理：增加角色资源关联表，生成持久层和服务端代码

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -40,6 +40,24 @@ insert into resource values ('010201','资源管理保存/显示',null,'["/syste
 insert into resource values ('0103','角色管理','/system/role',null,'01');
 insert into resource values ('010301','角色管理角色/权限管理',null,'["/system/admin/role"]','0103');
 
+drop table if exists `role_resource`;
+create table `role_resource`(
+                       id char(8) not null default '' comment 'id',
+                       role_id char(8) not null comment '角色|id',
+                       resource_id char(6) not null comment '资源|id',
+                       primary key (id)
+)engine=innodb default charset=utf8mb4 comment ='角色资源关联';
+insert into `role_resource` values ('00000000','00000000','01');
+insert into `role_resource` values ('00000001','00000000','0101');
+insert into `role_resource` values ('00000002','00000000','010101');
+insert into `role_resource` values ('00000003','00000000','010102');
+insert into `role_resource` values ('00000004','00000000','010103');
+insert into `role_resource` values ('00000005','00000000','0102');
+insert into `role_resource` values ('00000006','00000000','010201');
+insert into `role_resource` values ('00000007','00000000','0103');
+insert into `role_resource` values ('00000008','00000000','010301');
+
+
 # 课程内容文件
 drop table if exists course_content_file;
 create table course_content_file(
