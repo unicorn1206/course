fac4aa8f45cfbf41c4f6a41b1d8e7baa94d85d00
nemoeckel <stephanie_eckel@126.com>
2020-12-07 21:28:24 +0800

    1.网站开发，修改底部及主页面文案

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -3,11 +3,11 @@
 
         <section class="jumbotron text-center">
             <div class="container">
-                <h1>Album example1</h1>
+                <h1>web课程</h1>
                 <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                 <p>
-                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                    <a href="#" class="btn btn-primary my-2 p-3 font-weight-bold">点击进入课程</a>
+
                 </p>
             </div>
         </section>
