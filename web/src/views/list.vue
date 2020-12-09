987b358f26e084e3d6229db210578925df0221f0
nemoeckel <stephanie_eckel@126.com>
2020-12-09 22:33:28 +0800

    1.网站首页开发，增加课程分类展示

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -1,7 +1,37 @@
 <template>
     <main role="main">
+        <div class="header-nav">
+            <div class="clearfix">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <a v-on:click="onClickLevel1('00000000')" id="category-00000000" href="javascript:;" class="cur">全部</a>
+                            <a v-for="o in level1" v-bind:key="o.name" v-on:click="onClickLevel1(o.id)" v-bind:id="'category-' + o.id" href="javascript:;">{{o.name}}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="skill clearfix">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <a v-on:click="onClickLevel2('11111111')" id="category-11111111" href="javascript:;" class="on">不限</a>
+                        <a v-for="o in level2" v-bind:key="o.name" v-on:click="onClickLevel2(o.id)" v-bind:id="'category-' + o.id" href="javascript:;">{{o.name}}</a>
+
+                        <div style="clear:both"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="album py-5 bg-light">
             <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <pagination ref="pagination" v-bind:list="listCourse"></pagination>
+                    </div>
+                </div>
+                <br>
                 <div class="row">
                     <div v-for="o in courses" v-bind:key="o.name" class="col-md-4">
                         <the-course v-bind:course="o"></the-course>
@@ -21,12 +51,16 @@
         components: {TheCourse},
         data:function () {
             return{
-                courses:[]
+                courses:[],
+                level1: [],
+                level2: [],
             }
         },
         mounted() {
             let _this = this;
-            _this.listCourse();
+            _this.$refs.pagination.size = 1;
+            _this.listCourse(1);
+            _this.allCategory();
         },
         methods:{
             /**
@@ -34,37 +68,115 @@
              */
             listCourse(page){
                 let _this = this;
-                Loading.show();
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list', {
                     page: page,
-                    size: 3
+                    size: _this.$refs.pagination.size,
                 }).then((response) => {
-                    Loading.hide();
                     let resp = response.data;
                     _this.courses = resp.content.list;
-                    //_this.$refs.pagination.render(page, resp.content.total);//重新渲染当前组件，所选页数的底色
+                    _this.$refs.pagination.render(page, resp.content.total);//重新渲染当前组件，所选页数的底色
                 }).catch((response) => {
                     console.log("error:",response);
                 })
-            }
+            },
+            /**
+             * 所有分类查询
+             */
+            allCategory() {
+                let _this = this;
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/category/all').then((response)=>{
+                    let resp = response.data;
+                    let categorys = resp.content;
+                    _this.categorys = categorys;
+
+                    // 将所有记录格式化成树形结构
+                    _this.level1 = [];
+                    for (let i = 0; i < categorys.length; i++) {
+                        let c = categorys[i];
+                        if (c.parent === '00000000') {
+                            _this.level1.push(c);
+                        } else {
+                            _this.level2.push(c);
+                        }
+                    }
+                })
+            },
         }
     }
 </script>
 
 <style scoped>
-    .title1{
-        margin-bottom: 2rem;
-        color: #fafafa;
-        letter-spacing: 0;
-        text-shadow: 0px 1px 0px #999,0px 2px 0px #888,0px 3px 0px #777,0px 4px 0px #666;
-        font-size:2rem;
+    /* 头部 一级分类 */
+    .header-nav {
+        height: auto;
+        background: #fff;
+        box-shadow: 0 8px 16px 0 rgba(28,31,33,.1);
+        padding: 16px 0;
+        box-sizing: border-box;
+        position: relative;
+        z-index: 1;
+        /*background-color: #d6e9c6;*/
     }
-    .title2{
-        margin-bottom: 2rem;
-        color: transparent;
-        -webkit-text-stroke: 1px black;
-        letter-spacing: 0.04em;
-        font-size:2rem;
+    .header-nav>div {
+        width: 100%;
+        padding-left: 12px;
+        box-sizing: border-box;
+        margin-left: auto;
+        margin-right: auto;
+        /*background-color: #B4D5AC;*/
     }
-
+    .header-nav a {
+        float: left;
+        font-size: 16px;
+        color: #07111b;
+        line-height: 50px;
+        height: 45px;
+        position: relative;
+        margin-right: 46px;
+        font-weight: 700;
+    }
+    .header-nav a:hover {
+        color: #c80;
+    }
+    .header-nav a.cur {
+        color: #c80;
+    }
+    .header-nav a.cur:before {
+        display: block;
+    }
+    .header-nav a:before {
+        display: none;
+        content: ' ';
+        position: absolute;
+        bottom: 0;
+        background: #c80;
+        width: 16px;
+        height: 3px;
+        left: 50%;
+        margin-left: -8px;
+    }
+    /* 二级分类 */
+    .skill {
+        width: 100%;
+        padding: 24px 0 0;
+        position: relative;
+        margin: 0 auto;
+    }
+    .skill a.on {
+        color: #c80;
+        background: rgba(204,136,0,.1);
+    }
+    .skill a {
+        float: left;
+        margin-right: 20px;
+        padding: 0 12px;
+        font-size: 14px;
+        color: #4d555d;
+        line-height: 32px;
+        border-radius: 6px;
+        margin-bottom: 12px;
+    }
+    .skill a:hover {
+        background: #d9dde1;
+    }
 </style>
